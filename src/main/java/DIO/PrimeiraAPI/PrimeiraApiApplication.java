5345e3a3f3a49c1739ff21cd0b8afba624e8278c
robsonrsc <robsonrsc.stos@gmail.com>
2022-11-23 22:01:20 +0000

    second commit

---
+++ src/main/java/DIO/PrimeiraAPI/PrimeiraApiApplication.java
@@ -0,0 +1,13 @@
+package DIO.PrimeiraAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiraApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiraApiApplication.class, args);
+	}
+
+}
